e613dd6e9b7453ae1603496f0a73d1e74fa6bd93
Maddi Joyce <maddi@maddijoyce>
2018-06-13 20:40:42 +1000

    Upload pre-zipped folder.

--- src/lambda.ts
+++ src/lambda.ts
@@ -12,7 +12,9 @@ const lambda = new AWS.Lambda();
 export const funcFolder = path.join(base, 'functions');
 
 export async function downloadAll() {
-  if (!fs.existsSync(funcFolder)) { fs.mkdirSync(funcFolder); }
+  if (!fs.existsSync(funcFolder)) {
+    fs.mkdirSync(funcFolder);
+  }
 
   const functions = (await lambda.listFunctions().promise()).Functions || [];
   for (const func of functions) {
@@ -20,26 +22,25 @@ export async function downloadAll() {
       fs.mkdirSync(path.join(funcFolder, func.FunctionName || '.'));
     }
 
-    fs.writeFileSync(path.join(funcFolder, func.FunctionName || '.', 'configuration.json'), JSON.stringify({
-      FunctionArn: func.FunctionArn,
-      FunctionName: func.FunctionName,
-      Description: func.Description,
-      Handler: func.Handler,
-      Runtime: func.Runtime,
-      Timeout: func.Timeout,
-      Role: func.Role,
-    }, null, 2));
+    fs.writeFileSync(
+      path.join(funcFolder, func.FunctionName || '.', 'configuration.json'),
+      JSON.stringify(
+        {
+          FunctionArn: func.FunctionArn,
+          FunctionName: func.FunctionName,
+          Description: func.Description,
+          Handler: func.Handler,
+          Runtime: func.Runtime,
+          Timeout: func.Timeout,
+          Role: func.Role
+        },
+        null,
+        2
+      )
+    );
 
     if (func.Role) {
       await downloadOne(func.Role);
-    }
-
-    const funcFile = await lambda.getFunction({ FunctionName: func.FunctionName || '' }).promise();
-    if (funcFile && funcFile.Code && funcFile.Code.Location) {
-      https.get(funcFile.Code.Location, (res) =>
-        res.pipe(unzip.Parse())
-          .on('entry', (entry) =>
-            entry.pipe(fs.createWriteStream(path.join(funcFolder, func.FunctionName || '.', entry.path)))));
     }
   }
 }
@@ -48,27 +49,31 @@ export async function uploadAll() {
   const files = fs.readdirSync(funcFolder);
 
   for (const file of files) {
-    const func = JSON.parse(fs.readFileSync(path.join(funcFolder, file, 'configuration.json'), 'utf8'));
-    await lambda.updateFunctionConfiguration({
-      FunctionName: func.FunctionName,
-      Description: func.Description,
-      Handler: func.Handler,
-      Runtime: func.Runtime,
-      Timeout: func.Timeout,
-      Role: func.Role,
-    }).promise();
+    const func = JSON.parse(
+      fs.readFileSync(path.join(funcFolder, file, 'configuration.json'), 'utf8')
+    );
+    await lambda
+      .updateFunctionConfiguration({
+        FunctionName: func.FunctionName,
+        Description: func.Description,
+        Handler: func.Handler,
+        Runtime: func.Runtime,
+        Timeout: func.Timeout,
+        Role: func.Role
+      })
+      .promise();
 
-    const funcFiles = fs.readdirSync(path.join(funcFolder, file)).filter((funcFile) => funcFile !== 'configuration.json');
-    const archive = new zip();
-    for (const funcFile of funcFiles) {
-      archive.addLocalFile(path.join(funcFolder, file, funcFile));
-    }
-    const zipFile = archive.toBuffer();
+    const zipFile = fs.readFileSync(
+      path.join(funcFolder, file, 'package.zip'),
+      'utf8'
+    );
 
-    await lambda.updateFunctionCode({
-      FunctionName: func.FunctionName,
-      Publish: true,
-      ZipFile: zipFile,
-    }).promise();
+    await lambda
+      .updateFunctionCode({
+        FunctionName: func.FunctionName,
+        Publish: true,
+        ZipFile: zipFile
+      })
+      .promise();
   }
 }
